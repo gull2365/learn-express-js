5bf63713a93bb1a8ceb14048282e0d963ae07ed3
gull2365 <jych2365@gmail.com>
2023-09-18 07:01:05 +0900

    learn: 다른 라우트 사용법

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in the moon");
+app.use("/", (req, res, next) => {
+  console.log("always runs");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("in another moon");
-  res.send("hello");
+  res.send(" add product");
 });
-
-const server = http.createServer(app);
-
-server.listen(3000);
+// express 내부 문서를 읽어보면 app.listen에서 서버를 같이 불러와줌 (http import 제거가 가능)
+app.listen(3000);
