38d26b7ed044aef9bfc3377627e0a59ea816a250
gull2365 <jych2365@gmail.com>
2023-09-18 07:37:41 +0900

    learn: express router 사용

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
+
+const adminRoute = require("./route/admin");
+const shopRoute = require("./route/shop");
 
 app.use(bodyParser.urlencoded({ extends: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 // express 내부 문서를 읽어보면 app.listen에서 서버를 같이 불러와줌 (http import 제거가 가능)
 app.listen(3000);
