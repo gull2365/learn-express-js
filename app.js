e10511e4a406dcfdd0a7c0f1f274890735c564e7
gull2365 <jych2365@gmail.com>
2023-09-18 07:18:22 +0900

    learn : 수신 요청 분석과 app.post로 미들웨어 실행 제한

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("always runs");
-  next();
-});
+app.use(bodyParser.urlencoded({ extends: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("in another moon");
-  res.send(" add product");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
+  );
 });
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 // express 내부 문서를 읽어보면 app.listen에서 서버를 같이 불러와줌 (http import 제거가 가능)
 app.listen(3000);
