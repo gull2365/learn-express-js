9c731a40ff814b5760e1fbb019e739dfa85d189b
Gull 2365 <jych2365@gmail.com>
2023-09-17 23:10:14 +0900

    learn: 미들웨어 추가, 작동방식

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 
 const app = express();
 
-const server = http.createServer();
+app.use((req, res, next) => {
+  console.log("in the moon");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in another moon");
+  res.send("hello");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
