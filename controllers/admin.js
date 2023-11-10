1778f39139735fa5da77b7a2588e40613d629c8c
gull2365 <jych2365@gmail.com>
2023-11-10 11:28:59 +0900

    feat: 제품 데이터 저장

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,7 +11,11 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const product = new Product(title, description, imageUrl, price);
   product.save();
   res.redirect("/");
 };
@@ -24,4 +28,4 @@ exports.getProducts = (req, res, next) => {
       path: "/admin/products",
     });
   });
-}
+};
