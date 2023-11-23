a7a291b16427e77e9803222298adf403c4142bcb
gull2365 <jych2365@gmail.com>
2023-11-23 12:44:12 +0900

    fix: editing product data

--- routes/admin.js
+++ routes/admin.js
@@ -17,6 +17,10 @@ router.post("/add-product", adminController.postAddProduct);
 
 router.get("/edit-product/:productId", adminController.getEditProduct);
 
-router.get("edit-product");
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post("/delete-product", adminController.postDeleteProduct);
+
+router.post("delete-product");
 
 module.exports = router;
