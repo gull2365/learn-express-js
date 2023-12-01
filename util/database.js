e1d57a6cb15d46660bbfe82f6f78b321e10d1b6c
gull2365 <jych2365@gmail.com>
2023-12-01 16:09:03 +0900

    feat: 모델 정의

--- util/database.js
+++ util/database.js
@@ -3,6 +3,8 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("node-complete", "root", "051101", {
   dialect: "mysql",
   host: "localhost",
-}); // schema, username, password, options
+}); // schema, username, password, options connection pool!
 
-module.exports = sequelize;
+//커넥션 풀은 데이터베이스와 연결된 커넥션을 미리 만들어 놓고 이를 pool로 관리하는 것이다. 즉, 필요할 때마다 커넥션 풀의 커넥션을 이용하고 반환하는 기법이다. 이처럼 미리 만들어 놓은 커넥션을 이용하면 Connection에 필요한 비용을 줄일 수 있다. 따라서 DB에 빠르게 접속할 수 있다.
+
+https: module.exports = sequelize;
