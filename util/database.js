084412f5ad49621aa353606d75d3e4bf648f96d3
gull2365 <jych2365@gmail.com>
2023-12-01 15:55:28 +0900

    feat: 데이터 베이스에 연결

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "051101", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "051101",
-});
+}); // schema, username, password, options
 
-module.exports = pool.promise();
+module.exports = sequelize;
