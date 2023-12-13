66c9ebea059723f5cf3b8c22730fe0e688c5a4b5
gull2365 <jych2365@gmail.com>
2023-12-13 23:55:21 +0900

    feat: MongoDB 드라이버 설치

--- util/database.js
+++ util/database.js
@@ -1,10 +1,17 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "051101", {
-  dialect: "mysql",
-  host: "localhost",
-}); // schema, username, password, options connection pool!
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://jych2365:Mjung051101!@cluster0.xan7ok9.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-//커넥션 풀은 데이터베이스와 연결된 커넥션을 미리 만들어 놓고 이를 pool로 관리하는 것이다. 즉, 필요할 때마다 커넥션 풀의 커넥션을 이용하고 반환하는 기법이다. 이처럼 미리 만들어 놓은 커넥션을 이용하면 Connection에 필요한 비용을 줄일 수 있다. 따라서 DB에 빠르게 접속할 수 있다.
-
-https: module.exports = sequelize;
+module.exports = mongoConnect;
