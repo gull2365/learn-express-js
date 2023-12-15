04ef27eb75a362688407d7de4b9d5e12cc22568b
gull2365 <jych2365@gmail.com>
2023-12-15 15:33:36 +0900

    feat: 데이터베이스 연결 완료하기

--- util/database.js
+++ util/database.js
@@ -1,17 +1,30 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://jych2365:Mjung051101!@cluster0.xan7ok9.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://jych2365:Mjung051101!@cluster0.xan7ok9.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected!");
+      _db = client.db();
       callback(client);
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
 module.exports = mongoConnect;
+module.exports.getDb = getDb;
+```
