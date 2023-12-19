7769eac8f805389cd601766d7613e103032e7850
gull2365 <jych2365@gmail.com>
2023-12-19 16:22:03 +0900

    caf: 3m 00s

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("6581399533278a37294489a6")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => {
