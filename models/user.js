d363c562b624e042753e7d27bd35d8877a739489
gull2365 <jych2365@gmail.com>
2023-12-19 16:08:08 +0900

    feat: 장바구니 항목 및 주문 작업

--- models/user.js
+++ models/user.js
@@ -4,14 +4,25 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
     this.email = email;
+    this.cart = cart;
+    this.id = _id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection("users").updateOne(
+      { id: new ObjectId(this, id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
